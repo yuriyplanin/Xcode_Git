6c8de06fe0d7de16b00bf4b2234d20d203ffafb5
Yuriy Planin <planinyuriy@gmail.com>
2022-08-08 21:37:33 +0300

    Initial Commit

---
+++ Lesson12_GitTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Lesson12_GitTest
+//
+//  Created by Yuriy Planin on 8.08.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
